985082ee737d35033de0986f44030a7facde84e7
Michele Schäuble <michele.schaeuble@gmail.com>
2013-04-12 11:32:57 +0200

    added Consts class

---
+++ src/test/java/com/github/mschaeuble/aptizer/util/Consts.java
@@ -0,0 +1,15 @@
+package com.github.mschaeuble.aptizer.util;
+
+
+public final class Consts {
+
+  public static final String NEW_LINE = "\n";
+  
+  public static final String SPACE = " ";
+  
+  public static final String ASTERISK = "*";
+  
+  public static final String INDENTATION = "  ";
+
+  public static final String THREE_DASHES = "---";
+}
